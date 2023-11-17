038ce2717c66a02fd6cba5293d971efda45e2e7e
Tom Underhill <tomunderhill6@gmail.com>
2023-11-17 21:42:27 +0000

    Solves 909c

--- div3/909/c.py
+++ div3/909/c.py
@@ -5,20 +5,21 @@ for i in range(t):
   n = int(input())
   arr = list(map(int, input().split()))
   
-  sums = arr[:]
-  max_sums = arr[:]
-  valid_start = 0
+  curr_sum = arr[0]
+  biggest_sum = arr[0]
   
   for i in range(1, n):
-    if (arr[i - 1] % 2 != arr[i] % 2):
+    if (arr[i - 1] % 2 != arr[i] % 2 and curr_sum > 0):
+      curr_sum += arr[i]
       # print(arr[i])
-      for j in range(valid_start, i):
-        sums[j] += arr[i]
-        max_sums[j] = max(sums[j], max_sums[j])
+        
     else:
+      curr_sum = arr[i]
       # print("here")
-      valid_start = i
+    
+    if (curr_sum > biggest_sum):
+      biggest_sum = curr_sum
       
-  # print(sums)
-  # print(max_sums)
-  print(max(max_sums))
+  # if (curr_sum > biggest_sum):
+  #   biggest_sum = curr_sum
+  print(biggest_sum)
