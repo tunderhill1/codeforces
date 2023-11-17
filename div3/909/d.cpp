c337876aa2eacf5dc63f1cf1a88608a77e3832ff
Tom Underhill <tomunderhill6@gmail.com>
2023-11-17 22:31:24 +0000

    Solves 909d in C++

--- div3/909/d.cpp
+++ div3/909/d.cpp
@@ -2,33 +2,41 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h> 
- 
+#include <map>
+
 int a[200000];
 double k[200000];
 
 void solve() {
   int n;
   
+  std::map<double, int> my_map;
   // get inputs
   scanf("%d", &n);
   for (int i = 0; i < n; i ++) {
     scanf("%d", &a[i]);
-    k[i] = log2(a[i]);
+    k[i] = a[i] - log2(a[i]);
+    my_map[k[i]] = -1;
     // printf("%d, %f\n", a[i], k[i]);
   }
 
-  int count = 0;
   for (int i = 0; i < n; i ++) {
-    for (int j = i + 1; j < n; j ++) {
-      if (a[j] + k[i] == a[i] + k[j]) {
-        // printf("aj: %d, ki: %f ai: %d kj: %f\n", a[j], k[i], a[i], k[j]);
-        count++;
-      }
-    }
+    my_map[k[i]] += 1;
+
+    // for (int j = i + 1; j < n; j ++) {
+    //   if (a[j] + k[i] == a[i] + k[j]) {
+    //     // printf("aj: %d, ki: %f ai: %d kj: %f\n", a[j], k[i], a[i], k[j]);
+    //     count++;
+    //   }
+    // }
+  }
+  long long count = 0;
+  for (const auto& [key, c] : my_map) {
+    count += (c / 2.0) * (c + 1) ;
   }
 
   // return output
-  printf("%d\n", count);
+  printf("%lld\n", count);
 }
 
 int main()
