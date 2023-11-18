98e59cc8f839dab63b29891a3bad24201835e93d
Tom Underhill <tomunderhill6@gmail.com>
2023-11-18 22:41:01 +0000

    901d before new idea

--- div2/901/d.cpp
+++ div2/901/d.cpp
@@ -16,6 +16,9 @@ using namespace std;
 #include <random>
 
 typedef pair<int, int> pii;
+typedef pair<int, bool> pib;
+
+int get_m(int i);
 
 void solve() {
   int n, x;
@@ -42,21 +45,43 @@ void solve() {
   }
 
   // gets i_mex_to_m 
-  // i_mex_to_m(i, mex) gets m for a[0:i] with starting mex of mex
-  map<pii, int> i_mex_to_m;
-  for (int mex = 1; mex <= start_mex; mex ++) {
-    i_mex_to_m[pii(0, mex)] = counts[0] * mex;
-  }
+  // i_to_m(i, true) gets m for a[0:i] with starting mex of i + 1
+  // i_to_m(i, false) gets m for a[0:i] with starting mex of start_mex
+
+  map<pib, int> i_to_m;
+  i_to_m[pib(0, true)] = counts[0] - 1;
+  i_to_m[pib(0, false)] = (counts[0] - 1) * start_mex;
+  get_m(start_mex);
 
   for (int i = 1; i < start_mex; i ++) {
-    for (int mex = i + 1; mex <= start_mex; mex ++) {
-      int with_i = mex * counts[i] + i_mex_to_m[pii(i - 1, i)];
-      int without_i = i_mex_to_m[pii(i - 1, mex)];
-      i_mex_to_m[pii(i, mex)] = with_i < without_i ? with_i : without_i;
-    }
+    int without_i = i_to_m[pib(i - 1, false)];
+
+    int with_i_and_next_mex = (i + 1) * (counts[i] - 1) + i_to_m[pib(i - 1, true)] + i;
+    i_to_m[pib(i, true)] = with_i_and_next_mex < without_i ? with_i_and_next_mex : without_i;
+
+    int with_i_and_start_mex = start_mex * (counts[i] - 1) + i_to_m[pib(i - 1, true)] + i;
+    i_to_m[pib(i, false)] =  with_i_and_start_mex < without_i ? with_i_and_start_mex : without_i;
+
+    // for (int mex = i + 1; mex <= start_mex; mex ++) {
+    //   int with_i = mex * counts[i] + i_to_m[pib(i - 1, true)];
+    //   int without_i = i_to_m[pib(i - 1, false)];
+    //   i_to_m[pib(i, mex)] = with_i < without_i ? with_i : without_i;
+    // }
   }
 
-  cout << i_mex_to_m[pii(start_mex - 1, start_mex)] - start_mex << "\n";
+  cout << i_to_m[pib(start_mex - 1, true)] << "\n";
+}
+
+int get_m(int i, int last_mex) {
+  int with_i = last_mex * (counts[i]) + get_mex(i - 1, i);
+  int without_i = get_mex(i - 1, last_mex);
+  return with_i < without_i ? with_i : without_i;
+}
+
+struct sub_a {
+  int m;
+  int curr_mex;
+  
 }
 
 int main()
