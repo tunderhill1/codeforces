704528b10d1b45826751c3973c0bd38569146817
Tom Underhill <tomunderhill6@gmail.com>
2023-11-18 17:26:16 +0000

    Implements template for c++

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <map>
+
+void solve() {
+  int n;
+  scanf("%d", &n);
+  
+  int result = 0;
+
+  printf("%lld\n", result);
+}
+
+int main()
+{
+  int num_test_cases;
+  scanf("%d", &num_test_cases);
+
+  for (int test_case = 0; test_case < num_test_cases; test_case ++) {
+    solve();
+  }
+
+  return 0;
+}
