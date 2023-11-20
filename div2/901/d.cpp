c1008ed491c6557e5b0c6b8946fca710634ef7e8
Tom Underhill <thunderhill007@gmail.com>
2023-11-20 21:50:53 +0000

    Implementation of 901d

--- div2/901/d.cpp
+++ div2/901/d.cpp
@@ -14,11 +14,9 @@ using namespace std;
 #include <queue>
 #include <unordered_set>
 #include <random>
+#include <climits>
 
 typedef pair<int, int> pii;
-typedef pair<int, bool> pib;
-
-int get_m(int i);
 
 void solve() {
   int n, x;
@@ -34,54 +32,69 @@ void solve() {
     counts[x]++;
   }
 
+  if (counts[0] == 0) {
+    cout << 0 << "\n";
+    return;
+  }
+
+
   // gets start_mex
   int start_mex = 0;
+  int curr_count = counts[0] + 1;
+  bool erase = false;
+  int to_erase = -1;
   for ( const auto &[i, c] : counts ) {
+    if (erase) {
+      counts.erase(to_erase);
+    }
     if (i == start_mex) {
       start_mex ++;
+      if (c >= curr_count) {
+        to_erase = i;
+        erase = true;
+      } else {
+        erase = false;
+        curr_count = c;
+      }
     } else {
+      if (c >= curr_count) {
+        counts.erase(i);
+      }
       break;
     }
   }
 
   // gets i_mex_to_m 
-  // i_to_m(i, true) gets m for a[0:i] with starting mex of i + 1
-  // i_to_m(i, false) gets m for a[0:i] with starting mex of start_mex
+  // i_mex_to_m(i, mex) gets m for a[0:i] with starting mex of mex 
+  // and is your first turn
 
-  map<pib, int> i_to_m;
-  i_to_m[pib(0, true)] = counts[0] - 1;
-  i_to_m[pib(0, false)] = (counts[0] - 1) * start_mex;
-  get_m(start_mex);
-
-  for (int i = 1; i < start_mex; i ++) {
-    int without_i = i_to_m[pib(i - 1, false)];
-
-    int with_i_and_next_mex = (i + 1) * (counts[i] - 1) + i_to_m[pib(i - 1, true)] + i;
-    i_to_m[pib(i, true)] = with_i_and_next_mex < without_i ? with_i_and_next_mex : without_i;
-
-    int with_i_and_start_mex = start_mex * (counts[i] - 1) + i_to_m[pib(i - 1, true)] + i;
-    i_to_m[pib(i, false)] =  with_i_and_start_mex < without_i ? with_i_and_start_mex : without_i;
-
-    // for (int mex = i + 1; mex <= start_mex; mex ++) {
-    //   int with_i = mex * counts[i] + i_to_m[pib(i - 1, true)];
-    //   int without_i = i_to_m[pib(i - 1, false)];
-    //   i_to_m[pib(i, mex)] = with_i < without_i ? with_i : without_i;
-    // }
+  // base case: i = 0
+  map<pii, int> i_mex_to_m;
+  for (int mex = 0; mex <= start_mex; mex ++) {
+    i_mex_to_m[pii(0, mex)] = (counts[0] - 1) * mex;
   }
 
-  cout << i_to_m[pib(start_mex - 1, true)] << "\n";
-}
+  // fill table
+  int prev_i = 0;
+  int lowest_m = i_mex_to_m[pii(0, start_mex)];
+  counts.erase(0);
+  for (const auto &[i, c] : counts ) {
+    if (i < start_mex) {
+      // time improvement: can just iterate over is which are in counts
+      for (int mex = i + 1; mex <= start_mex; mex ++) {
+        int with_i = mex * (c - 1) + i_mex_to_m[pii(prev_i, i)] + i;
+        int without_i = i_mex_to_m[pii(prev_i, mex)];
+        i_mex_to_m[pii(i, mex)] = with_i < without_i ? with_i : without_i;
+      }
+      if (i_mex_to_m[pii(i, start_mex)] < lowest_m) {
+        lowest_m = i_mex_to_m[pii(i, start_mex)];
+      }
+      int prev_i = i;
+    }
+  }
 
-int get_m(int i, int last_mex) {
-  int with_i = last_mex * (counts[i]) + get_mex(i - 1, i);
-  int without_i = get_mex(i - 1, last_mex);
-  return with_i < without_i ? with_i : without_i;
-}
-
-struct sub_a {
-  int m;
-  int curr_mex;
-  
+  // subtract start_mex, because remove first, before point penalty
+  cout << lowest_m << "\n";
 }
 
 int main()
